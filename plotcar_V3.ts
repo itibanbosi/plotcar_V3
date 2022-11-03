ff5bf1cbc9657295554d11e218e122812c2cf738
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-11-03 12:03:57 +0900

    更新 plotcar_V3.ts

--- plotcar_V3.ts
+++ plotcar_V3.ts
@@ -153,7 +153,7 @@ let original_bit = 0b1000     /* 1000 */
  
 */
 let outputsR = [DigitalPin.P7, DigitalPin.P6, DigitalPin.P4, DigitalPin.P3]
-let outputsL = [DigitalPin.P13, DigitalPin.P14, DigitalPin.P15, DigitalPin.P16];
+let outputsL = [DigitalPin.P16, DigitalPin.P15, DigitalPin.P14, DigitalPin.P13];
 
 //LED不使用
 led.enable(false)
