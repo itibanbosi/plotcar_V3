1fa1ea442401b74a97160c0f2b515bf45762827e
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-11-03 10:36:01 +0900

    更新 plotcar_V3.ts

--- plotcar_V3.ts
+++ plotcar_V3.ts
@@ -426,7 +426,7 @@ namespace eureka_plotter_car {
         for (let index = 0; index < kyori_seisuu; index++) {
             for (let n = 0; n < 4; n++) {
                 for (let m = 0; m < 4; m++) {
-
+                    serial.writeLine("" + Stepping_L);
                     pins.digitalWritePin(outputsL[m], (((Stepping_L >> 24 - n * 4) & (original_bit >> m)) >> (3-m)));
                     pins.digitalWritePin(outputsR[m], (((Stepping_R >> 24 - n * 4) & (original_bit >> m)) >> (3-m)));
                 }
