7e8c827d5b87fb6c6005f21dcd2b2bc50664b4e6
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-11-03 17:46:54 +0900

    更新 plotcar_V3.ts

--- plotcar_V3.ts
+++ plotcar_V3.ts
@@ -386,8 +386,8 @@ namespace eureka_plotter_car {
 
 
 
-    //% color="#ff3d03" weight=34 blockId=plotcar_speed block="motion speed slow|%plotcarspeed|fast" group="4 Default setting"
-    //% plotcarspeed.min=-90 plotcarspeed.max=3000
+    //% color="#ff3d03" weight=34 blockId=plotcar_speed block="motion speed fast|%plotcarspeed|slow" group="4 Default setting"
+    //% plotcarspeed.min=-80 plotcarspeed.max=2000
     export function plotcar_speed(plotcarspeed:number) :void{
 
         microbit_wait = microbit_wait*(100+plotcarspeed)/10
